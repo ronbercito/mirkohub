3dd054a11a6ee8ba2605380bc28b897773675bb6
ronbercito <rj.ston1985@gmail.com>
2026-09-06 09:09:31 -0500

    Ordenar entrada del submódulo Cajas NAP

---
+++ frontend/src/modules/red/nap_boxes/NapBoxes.jsx
@@ -0,0 +1,11 @@
+/**
+ * Archivo: frontend/src/modules/red/nap_boxes/NapBoxes.jsx
+ * Función: Entrada organizada del submódulo Cajas NAP.
+ * Trabaja con: ../NapBoxes.jsx, components/layout/Layout.jsx.
+ */
+import React from "react";
+import NapBoxesPage from "../NapBoxes";
+
+export default function NapBoxes() {
+  return <NapBoxesPage />;
+}

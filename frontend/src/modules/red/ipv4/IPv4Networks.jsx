5d2094870a35d013128f76ceb6400a0fd3750c23
ronbercito <rj.ston1985@gmail.com>
2026-09-06 09:09:22 -0500

    Ordenar entrada del submódulo IPv4

---
+++ frontend/src/modules/red/ipv4/IPv4Networks.jsx
@@ -0,0 +1,11 @@
+/**
+ * Archivo: frontend/src/modules/red/ipv4/IPv4Networks.jsx
+ * Función: Entrada organizada del submódulo Redes IPv4.
+ * Trabaja con: ../IPv4Networks.jsx, components/layout/Layout.jsx.
+ */
+import React from "react";
+import IPv4NetworksPage from "../IPv4Networks";
+
+export default function IPv4Networks() {
+  return <IPv4NetworksPage />;
+}
